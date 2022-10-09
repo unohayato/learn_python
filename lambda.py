d8adcd29734efeb4b75e31acacf51ce39d39395c
unohayato <mamidori1p1p@gmail.com>
2022-10-09 16:22:42 +0900

    lambda filter

--- lambda.py
+++ lambda.py
@@ -1,3 +1,4 @@
+import numbers
 from pprint import pformat
 from unittest import result
 
@@ -11,3 +12,9 @@ n1 = ['鈴木', '田中', '佐藤']
 n2 = ['太郎', '二郎', '三郎']
 result = list(map(lambda x, y: x + y + 'さん', n1 ,n2))
 print(result)
+
+
+numbers = [2, 4, 6, 8, 10, 12]
+
+result2 = list(filter(lambda x: x >= 10, numbers))
+print(result2)
