4c5cd1e379ad2a875ad1bc427b13f32744da196e
unohayato <mamidori1p1p@gmail.com>
2022-10-09 16:08:28 +0900

    zip

--- for.py
+++ for.py
@@ -1,6 +1,13 @@
-from tqdm import tqdm
+#from tqdm import tqdm
 
-for _ in tqdm(range(10 ** 9)):
-  pass
+#for _ in tqdm(range(10 ** 9)):
+#  pass
 
+sales_2020 = [4000, 5000, 6000]
+sales_2021 = [5000, 3000, 2000]
+
+for c, p in zip(sales_2021, sales_2020):
+  result = (c / p - 1) * 100
+  print(f'{result:.1f}%')
+  
 
