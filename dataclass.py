e64d72d93cefc7718dd084ffe59d5817a0504d29
unohayato <mamidori1p1p@gmail.com>
2022-10-14 22:55:37 +0900

    Update dataclass.py

--- dataclass.py
+++ dataclass.py
@@ -11,3 +11,13 @@ user = User('sato', 20)
 print(user.name)
 print(user.age)
 print(user.items)
+
+class User2:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  
+user1 = User2('u', 20)
+user2 = User2('u', 20)
+print(user1 == user2)
+
