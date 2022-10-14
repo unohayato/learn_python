034742733f6bc4e1b689a0a0760703a9aae660d7
unohayato <mamidori1p1p@gmail.com>
2022-10-14 22:49:51 +0900

    ミュータブル　デコレーター

--- dataclass.py
+++ dataclass.py
@@ -1,11 +1,13 @@
 
-from dataclasses import dataclass
+from dataclasses import field, dataclass
 
 @dataclass
 class User:
   name: str
   age: int
+  items: list[int] = field(default_factory=list)
   
 user = User('sato', 20)
 print(user.name)
 print(user.age)
+print(user.items)
